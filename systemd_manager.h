f2f42be5db44fd55508929054995d457f41fd597
Joris Offouga <offougajoris@gmail.com>
2020-05-16 15:47:02 +0200

    systemd_manager: implement isActive
    return True if the service is active and false otherwise
    
    Signed-off-by: Joris Offouga <offougajoris@gmail.com>

--- systemd_manager.h
+++ systemd_manager.h
@@ -1,7 +1,6 @@
 #ifndef SYSTEMD_MANAGER_H
 #define SYSTEMD_MANAGER_H
 
-
 #include <systemd/sd-daemon.h>
 #include <systemd/sd-bus.h>
 #include <stdbool.h>
@@ -13,22 +12,23 @@
 #define SYSTEMD_OBJECT_PATH_NAME "/org/freedesktop/systemd1"
 
 #define SYSTEMD_UNIT_INTERFACE_NAME "org.freedesktop.systemd1.Unit"
-#define SYSTEMD_SERVICE_INTERFACE_NAME  "org.freedesktop.systemd1.Service"
+#define SYSTEMD_SERVICE_INTERFACE_NAME "org.freedesktop.systemd1.Service"
 #define SYSTEMD_MANAGER_INTERFACE_NAME "org.freedesktop.systemd1.Manager"
 
 #define SYSTEMD_START_METHOD "StartUnit"
 #define SYSTEMD_STOP_METHOD "StopUnit"
 #define SYSTEMD_LOAD_UNIT_METHOD "LoadUnit"
 
-class SystemdManager{
+class SystemdManager
+{
 public:
     SystemdManager();
     ~SystemdManager();
     int startService(std::string unit_name, bool stop);
     bool isActive(std::string unit_name);
+
 private:
     sd_bus *m_bus = nullptr;
 };
-
 
 #endif // SYSTEMD_MANAGER_H
